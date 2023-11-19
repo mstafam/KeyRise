11b1b3e4997696d6cb9ca90eae96f028dd0c42bc
Mustafa Mohamed <mohamedm56@mymacewan.ca>
2023-11-18 19:02:20 -0700

    Centered Levels and Endscreen in Terminal

--- Rotate.c
+++ Rotate.c
@@ -1,20 +1,32 @@
+// libraries
 # include <stdio.h>
 # include <ncurses.h>
 # include "RotateFunc.h"
 
 int main() {
+	// Creating level variables
 	int levelOne = 1;
 	int levelTwo = 2;
 	int levelThree = 3;
+	// Initializing screen
 	initscr();
+	// Calling splash screen
 	splashscreen();
 	getch();
+	// Calling level one
 	display_level(levelOne);
 	getch();
+	// Calling level two
 	display_level(levelTwo);
 	getch();
+	// Calling level three
 	display_level(levelThree);
 	getch();
+	// Calling the endscreen
+	endscreen();
+	getch();
+	// Closing window
 	endwin();
+	// Return 0
 	return 0;
 }
--- RotateFunc.c
+++ RotateFunc.c
@@ -1,3 +1,4 @@
+// libraries
 # include <stdio.h>
 # include <ncurses.h>
 # include "RotateFunc.h"
@@ -7,46 +8,59 @@ void splashscreen() {
 }
 
 void display_level(int i) {
+	// Calculting Max screen dimensions
+	int yMax, xMax;
+	getmaxyx(stdscr, yMax, xMax);
 	if (i == 1) {
-		level1();
+	// Callind level1, if i is 1
+		level1(yMax, xMax);
 	} else if (i == 2) {
-		level2();
+	// Calling level2, if i is 2
+		level2(yMax, xMax);
 	} else if (i == 3) {
-		level3();
+	// Calling level3, if is 3
+		level3(yMax, xMax);
 	}
 }
 
 void endscreen() {
-	WINDOW *endscreen = newwin(30, 30, 0, 0);
+	// Calculating max screen dimensions
+	int yMax, xMax;
+	getmaxyx(stdscr, yMax, xMax);
+	// Creating endscreen window and box
+	WINDOW *endscreen = newwin(30, 80, yMax/2 - 15, xMax/2 - 40);
         refresh();
-        box(endscreen, '|', '-');
+        box(endscreen, 0, 0);
         wrefresh(endscreen);
         mvwprintw(endscreen, 1, 1, "End Screen");
         wrefresh(endscreen);
 }
 
-void level1() {
-	WINDOW *level1 = newwin(30, 30, 0, 0);
+void level1(int yMax, int xMax) {
+	// Creating level1 window and box
+	WINDOW *level1 = newwin(30, 80, yMax/2 - 15, xMax/2 - 40);
 	refresh();
-	box(level1, '|', '-');
+	box(level1, 0, 0);
 	wrefresh(level1);
 	mvwprintw(level1, 1, 1, "Level 1");
 	wrefresh(level1);
 }
 
-void level2() {
-	WINDOW *level2 = newwin(30, 30, 0,0);
+void level2(int yMax, int xMax) {
+	// Creating level2 window and box
+	WINDOW *level2 = newwin(30, 80, yMax/2 - 15, xMax/2 - 40);
 	refresh();
-	box(level2, '|', '-');
+	box(level2, 0, 0);
 	wrefresh(level2);
 	mvwprintw(level2, 1, 1, "Level 2");
 	wrefresh(level2);
 }
 
-void level3() {
-	WINDOW *level3 = newwin(30, 30, 0,0);
+void level3(int yMax, int xMax) {
+	// Creating level3 window and box
+	WINDOW *level3 = newwin(30, 80, yMax/2 - 15, xMax/2 - 40);
 	refresh();
-	box(level3, '|', '-');
+	box(level3, 0, 0);
 	wrefresh(level3);
 	mvwprintw(level3, 1, 1, "Level 3");
 	wrefresh(level3);
