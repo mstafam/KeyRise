703b321b1674b301a0067303668b96e7f8405e1d
Sami <samielsayed39@gmail.com>
2023-11-20 21:21:53 -0700

    Documentation and tweaked splash screen

--- RotateFunc.h
+++ RotateFunc.h
@@ -2,43 +2,47 @@
 
 
 /*
- * purpose:
- * params:
- * return:
-*/
+ * purpose: Display the splash screen with game title and menu options
+ * params:  None
+ * return:  Void
+ */
 void splashscreen();
 
 /*
- * purpose:
- * params:
- * return:
-*/
+ * purpose: Display a specific game level based on the level number provided
+ * params:  int i - The level number to display (1, 2, or 3).
+ * return:  Void
+ */
 void display_level(int i);
 
 /*
- * purpose:
- * params:
- * return:
-*/
+ * purpose: Display the end screen when the game is completed
+ * params:  None
+ * return:  Void
+ */
 void endscreen();
 
 /*
- * purpose:
- * params:
- * return:
-*/
-void level1();
+ * purpose: Display the first level of the game
+ * params:  int yMax - The maximum Y dimension of the screen
+ *          int xMax - The maximum X dimension of the screen
+ * return:  Void
+ */
+void level1(int yMax, int xMax);
 
 /*
- * purpose:
- * params:
- * return:
-*/
-void level2();
+ * purpose: Display the second level of the game
+ * params:  int yMax - The maximum Y dimension of the screen
+ *          int xMax - The maximum X dimension of the screen
+ * return:  Void
+ */
+void level2(int yMax, int xMax);
 
 /*
- * purpose:
- * params:
- * return:
-*/
-void level3();
+ * purpose: Display the third level of the game
+ * params:  int yMax - The maximum Y dimension of the screen
+ *          int xMax - The maximum X dimension of the screen
+ * return:  Void
+ */
+void level3(int yMax, int xMax);
+
